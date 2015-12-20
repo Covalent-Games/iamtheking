15271a615f32948d5ba61a8775af63cdf0d21a8d
Justin Smith <jtsmith1287@gmail.com>
2015-12-20 15:21:17 -0800

    Alert no longer removed if a hero leaves it unfinished. https://app.asana.com/0/75208819992288/75208819992339

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -69,32 +69,27 @@ public class Hero : MonoBehaviour {
 						currentQuest.Destination, 
 						Time.deltaTime * GameManager.HeroSpeed);
 				yield return null;
-			}
+			} 
 			// Work towards completing the objective.
 			if (currentQuest.Alert != null) {
 				switch (currentQuest.Goal) {
 					case QuestObject.QuestType.Slayer:
 						while (currentQuest.Quantity > 0) {
 							yield return new WaitForSeconds(2f);
-							float winChance = Mathf.Clamp(Strength / currentQuest.Difficulty, .01f, .90f);
-							float roll = UnityEngine.Random.value;
-							if (roll <= winChance) {
-								Debug.Log(string.Format("{0} rolled a {1} against {2} and has slayn a foe!",
-									Name, roll, winChance));
-								currentQuest.Quantity--;
-								Exp++;
-							} else {
-								Debug.Log(Name + " has taken damage!");
-								Health--;
-							}
-						}
+							AttackEnemy(currentQuest);
+						} 
 						break;
 				}
-				GameManager.Instance.RemoveAlert(currentQuest.Alert.AlertID);
+				if (currentQuest.Alert.Quantity <= 0) {
+					GameManager.Instance.RemoveAlert(currentQuest.Alert.AlertID); 
+				}
 			}
+			// We've accomplished the hero specific objective so remove the QuestObject.
+			// The alert/event may or may not be completed.
 			Quests.Remove(currentQuest);
 			yield return null;
-		}
+		} 
+		
 		//Return to the castle because all objectives are complete.
 		while (transform.position != Map.KingdomCastle.transform.position) {
 			transform.position =
@@ -106,5 +101,22 @@ public class Hero : MonoBehaviour {
 		}
 		Idle = true;
 		gameObject.SetActive(false);
+	}
+
+	private void AttackEnemy(QuestObject currentQuest) {
+
+		float winChance = Mathf.Clamp(Strength / currentQuest.Difficulty, .01f, .90f);
+		float roll = UnityEngine.Random.value;
+		if (roll <= winChance) {
+			Debug.Log(string.Format("{0} rolled a {1} against {2} and has slayn a foe!",
+				Name, roll, winChance));
+			// Update the currentQuest tracker, and also remove the enemy from the alert.
+			currentQuest.Quantity--;
+			currentQuest.Alert.Quantity--;
+			Exp++;
+		} else {
+			Debug.Log(Name + " has taken damage!");
+			Health--;
+		}
 	}
 }
